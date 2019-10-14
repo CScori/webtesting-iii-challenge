e24a561c8d74524a72cf3efc5f8cbda0c871b694
CScori <amilanotte1366@gmail.com>
2019-10-14 14:56:00 -0500

    all running snapshot tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,8 @@
-// Test away
+import react from 'react'
+import { render, fireEvent } from '@testing-library/react';
+
+import Dashboard from './Dashboard'
+
+test('running without errors', () => {
+    expect(render(<Dashboard />).toMatchSnapshot())
+ })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,7 @@
-// Test away!
+import react from 'react'
+import { render, fireEvent } from '@testing-library/react';
+import Display from './Display'
+
+test('Display running without errors', () => {
+    expect(render(<Display />).toMatchSnapshot())
+ })
