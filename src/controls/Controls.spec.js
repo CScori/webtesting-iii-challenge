1bfac58240cce56c73e6b2ad665048eea2b1d206
CScori <amilanotte1366@gmail.com>
2019-10-14 14:52:06 -0500

    adds snapshot test for control

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,4 +2,8 @@ import react from 'react'
 import { render, fireEvent } from '@testing-library/react';
 
 //import comp 
-
+import Controls from './Controls'
+//snapsheet test for chec
+test('running without errors', () => {
+   expect(render(<Controls />).toMatchSnapshot())
+})
