aa4a6e89d7707abad8795a29fec7a15620a9e4fe
CScori <amilanotte1366@gmail.com>
2019-10-14 16:16:47 -0500

    adds testing imports

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,14 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard'
+
+
+test('it renders dashboard correctly', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot()
+})
+
+// test('dashboard is default open closed', () => {
+//     const { getByText } = render(<Dashboard />)
+//     getByText(/open/i)
+//     })
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,3 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard'// Test away!
